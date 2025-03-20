2b5502b3bdf83237ab2bedb9475554dcf72d77be
v-AMusayelyan <162601436+v-AMusayelyan@users.noreply.github.com>
2025-03-20 11:10:44 +0400

    Initial Commit

---
+++ Dynamic Island/Dynamic_IslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_IslandApp.swift
+//  Dynamic Island
+//
+//  Created by Aren Musayelyan on 20.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_IslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
